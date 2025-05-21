13cf0ca301995eba26cf5092823495c80bde14e1
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-05-21 00:20:43 -0300

    config: create project and config tailwind and shadcn ui

---
+++ react/blog-set/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen min-w-full bg-slate-500">
+      <h1 className="text-4xl text-green-400">Title</h1>
+      <Button variant="secondary">Button</Button>
+    </div>
+  );
+}
