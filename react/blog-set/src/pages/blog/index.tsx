7edafa3c52c9b93440f09043c1541840dbe8fe15
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-05-21 21:20:27 -0300

    feat: create layout

--- react/blog-set/src/pages/blog/index.tsx
+++ react/blog-set/src/pages/blog/index.tsx
@@ -1,9 +1,7 @@
-import { Header } from "@/components/header";
 
 export default function Blog() {
   return (
     <div className="min-h-screen min-w-full bg-zinc-900">
-      <Header />
       <div>Blog</div>
     </div>
   )
--- react/blog-set/src/pages/index.tsx
+++ react/blog-set/src/pages/index.tsx
@@ -1,9 +1,8 @@
-import { Header } from "@/components/header";
 
 export default function Home() {
   return (
     <div className="min-h-screen min-w-full bg-zinc-900">
-      <Header />
+
     </div>
   );
 }
