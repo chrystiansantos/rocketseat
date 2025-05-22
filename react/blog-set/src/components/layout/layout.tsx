1ab109950ae7bc714d954b63f2a861e9ad339982
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-05-22 20:27:02 -0300

    feat: create hero

--- react/blog-set/src/components/layout/layout.tsx
+++ react/blog-set/src/components/layout/layout.tsx
@@ -1,22 +1,21 @@
 import { Inter } from 'next/font/google';
-import { ReactNode } from "react";
-import { Footer } from "../footer";
-import { Header } from "../header";
+import { Footer } from '../footer';
+import { Header } from '../header';
+
 type LayoutProps = {
-  children: ReactNode
-}
+  children: React.ReactNode;
+};
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export function Layout({ children }: LayoutProps) {
+export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className={`${inter.className} relative flex min-h-screen flex-col bg-gray-700`}>
+    <div
+      className={`${inter.className} relative flex min-h-screen flex-col bg-gray-700`}
+    >
       <Header />
-      <main className="flex-1 flex-col mb-12">
-        {children}
-      </main>
+      <main className="flex-1 flex flex-col mb-12">{children}</main>
       <Footer />
     </div>
-  )
-
-}
+  );
+};
