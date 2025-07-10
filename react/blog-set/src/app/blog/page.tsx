9974d511a322d0a1a586d6d19437e7bdf4cfc2b7
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-07-09 23:52:41 -0300

    feat: create page loading

--- react/blog-set/src/app/blog/page.tsx
+++ react/blog-set/src/app/blog/page.tsx
@@ -1,10 +1,16 @@
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
 
-export default function BlogListPage() {
+export default async function BlogListPage() {
   const sortedPosts = allPosts.sort((a, b) =>
     new Date(b.date).getTime() - new Date(a.date).getTime()
   )
+
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('')
+    }, 1000);
+  })
 
   return (
     <BlogList posts={sortedPosts} />
