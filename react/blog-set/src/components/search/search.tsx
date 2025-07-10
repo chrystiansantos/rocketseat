74980f4fd1364f0e91c5c95742fc018b1013bedc
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-07-09 22:43:01 -0300

    feat: create page not found

--- react/blog-set/src/components/search/search.tsx
+++ react/blog-set/src/components/search/search.tsx
@@ -1,12 +1,14 @@
 import { cn } from "@/lib/utils";
 import { CircleX, SearchIcon } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { ChangeEvent, useCallback } from "react";
+import { ChangeEvent, useCallback, useEffect, useRef } from "react";
 
 export function Search() {
+  const inputRef = useRef<HTMLInputElement | null>(null)
   const router = useRouter();
   const searchParams = useSearchParams()
   const query = searchParams?.get('q') ?? ''
+  const hasQuery = searchParams?.has('q')
 
   const handleSearch = useCallback((event: React.FormEvent) => {
     event.preventDefault()
@@ -28,10 +30,16 @@ export function Search() {
     })
   }
 
+  useEffect(() => {
+    if (hasQuery) inputRef.current?.focus()
+  }, [hasQuery])
+
+
   return (
     <form className="relative group w-full md:w-60" onSubmit={handleSearch}>
       <SearchIcon className={cn('text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 size-4 transition-colors duration-200 group-focus-within:text-blue-300', query ? 'text-blue-300' : '')} />
       <input
+        ref={inputRef}
         type="text"
         className="w-full h-10 md:w-60 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
         placeholder="Buscar"
