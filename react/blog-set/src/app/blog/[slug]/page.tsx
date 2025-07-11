8327ffc42803736e045ae080620bab042b052969
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-07-10 21:42:09 -0300

    feat: create page post

---
+++ react/blog-set/src/app/blog/[slug]/page.tsx
@@ -0,0 +1,22 @@
+import { PostPage } from "@/templates/blog"
+import { allPosts } from "contentlayer/generated"
+import { notFound } from "next/navigation"
+
+interface BlobPostPage {
+  params: Promise<{
+    slug: string
+  }>
+}
+
+export default async function BlogPostPage({ params }: BlobPostPage) {
+  const { slug } = (await params)
+  const post = allPosts.find(post => post.slug === slug)
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <PostPage post={post} />
+  )
+}
