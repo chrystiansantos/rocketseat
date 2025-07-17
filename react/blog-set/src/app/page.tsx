45476397ccdc9be8eb755954776ba410be8e3e8c
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-07-16 22:52:19 -0300

    feat: improving seo, home page

--- react/blog-set/src/app/page.tsx
+++ react/blog-set/src/app/page.tsx
@@ -1,4 +1,25 @@
 import { LandingPage } from "@/templates/landing-page";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Site.Set',
+  description: 'Venda sesu produtos como afiliado em um único lugar',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus producos como afiliado em um único lugar',
+    // url: '' // must have production control,
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [{
+      url: '', // must have production control,
+      width: 800,
+      height: 600,
+      alt: 'Site.Set'
+    }]
+  }
+}
 
 export default function Home() {
   return (
