78fe5e0b0a8207072c29fdb1c997fb018e4b1296
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-07-16 23:15:39 -0300

    feat: improving seo, blog page

--- react/blog-set/src/app/blog/page.tsx
+++ react/blog-set/src/app/blog/page.tsx
@@ -1,5 +1,26 @@
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estratégias para impulsioanr seu negócio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Blog',
+    description: 'Dicas e estratégias para impulsionar seu negócio',
+    // url: '' // must have production control,
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [{
+      url: '', // must have production control,
+      width: 800,
+      height: 600,
+      alt: 'Blog'
+    }]
+  }
+}
 
 export default async function BlogListPage() {
   const sortedPosts = allPosts.sort((a, b) =>
--- react/blog-set/src/app/page.tsx
+++ react/blog-set/src/app/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: 'Site.Set',
-  description: 'Venda sesu produtos como afiliado em um único lugar',
+  description: 'Venda seus produtos como afiliado em um único lugar',
   robots: 'index, follow',
   openGraph: {
     title: 'Site.Set',
