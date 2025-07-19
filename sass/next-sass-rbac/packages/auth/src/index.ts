74d23cafc9dedc5d700ddcd30122d2599d79b571
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-07-19 12:53:33 -0300

    feat: type subjects

---
+++ sass/next-sass-rbac/packages/auth/src/index.ts
@@ -0,0 +1,30 @@
+import {
+  AbilityBuilder,
+  type CreateAbility,
+  createMongoAbility,
+  type MongoAbility,
+} from '@casl/ability'
+
+import type { User } from './models/user'
+import { permissions } from './permissions'
+import type { ProjectSubject } from './subjects/project'
+import type { UserSubject } from './subjects/user'
+
+type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
+
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found`)
+  }
+
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+}
