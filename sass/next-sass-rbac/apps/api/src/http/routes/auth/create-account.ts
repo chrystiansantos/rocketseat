5254152b912e4ddc9e52e575e3aa19f09cb3df63
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-08-27 23:27:17 -0300

    feat: association user with organization

--- sass/next-sass-rbac/apps/api/src/http/routes/auth/create-account.ts
+++ sass/next-sass-rbac/apps/api/src/http/routes/auth/create-account.ts
@@ -28,10 +28,28 @@ export async function createAccount(app: FastifyInstance) {
           .send({ message: 'User with same e-mail already exists.' })
       }
 
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true
+        }
+      })
+
       const passwordHash = await hash(password, 6)
 
       await prisma.user.create({
-        data: { name, email, passwordHash, }
+        data: {
+          name,
+          email,
+          passwordHash,
+          member_on: autoJoinOrganization ? {
+            create: {
+              organizationId: autoJoinOrganization.id,
+            }
+          } : undefined
+        }
       })
 
       return reply.status(201).send('')
