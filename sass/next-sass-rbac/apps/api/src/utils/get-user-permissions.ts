8f742978b414635959297ab7ccea2f98ba99f8e7
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-12-03 23:04:09 -0300

    feat: update organization

---
+++ sass/next-sass-rbac/apps/api/src/utils/get-user-permissions.ts
@@ -0,0 +1,13 @@
+import { defineAbilityFor, userSchema, type Role } from "@saas/auth";
+
+export function getUserPermission(userId: string, role: Role) {
+
+  const authUser = userSchema.parse({
+    id: userId,
+    role: role
+  })
+
+  const ability = defineAbilityFor(authUser)
+
+  return ability
+}
